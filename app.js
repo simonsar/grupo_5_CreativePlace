e71f4ad38877e186700a3dbce379d73e1aa04227
lichu20 <lisa@delbono.com.ar>
2022-12-31 19:32:42 -0300

    solucionado

--- app.js
+++ app.js
@@ -8,12 +8,8 @@ app.listen(process.env.PORT || 3000, function(){
     console.log("Servidor corriendo");
 });
 
-app.get("/detalle-de-producto", (req, res) => {
-    res.sendFile(__dirname + '/views/detalle-de-producto.html')
-});
-
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/home.html')
+    res.sendFile(__dirname + '/views/home.html')
 });
 
 app.get("/detalle-de-producto", (req, res) => {
