9655b839e4a749591d860a90c349ca2233f27afd
lichu20 <lisa@delbono.com.ar>
2023-01-12 20:15:31 -0300

    carrito casi terminado

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Servidor corriendo");
