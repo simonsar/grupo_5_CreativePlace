0341b56ed54259e127cb656e0e3969b2a54629b4
simonsar <simonsara989@gmail.com>
2023-01-19 17:42:30 -0300

    app.set modificado

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require("express");
 const app = express();
 const mainRoutes = require('./src/routes/mainRouter');
 
-app.set("view engine", "ejs");
-
 app.use(express.static('public'));
 
 app.listen(process.env.PORT || 3000, function(){
