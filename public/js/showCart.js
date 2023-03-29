606dc799262be742da2c02be67cfdc90b7ed392a
simonsar <simonsara989@gmail.com>
2023-03-28 21:15:56 -0300

    carrito

--- public/js/showCart.js
+++ public/js/showCart.js
@@ -5,5 +5,17 @@ if (document.readyState == 'loading') {
 }
 
 function ready(){
+    if(JSON.parse(localStorage.getItem('productInCart')) == null || JSON.parse(localStorage.getItem('productInCart')).length == 0 ) {
+        emptyCart()
+    }else{
+        displayCart()
+    }
+}
+
+function emptyCart(){
+
+}
+
+function displayCart(){
     
 }
