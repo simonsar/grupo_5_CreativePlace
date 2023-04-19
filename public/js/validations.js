8f3c5a0e14446bdba67aa5baddf6fc6296536f68
simonsar <simonsara989@gmail.com>
2023-04-19 18:10:29 -0300

    algunos problemas validations

--- public/js/validations.js
+++ public/js/validations.js
@@ -4,31 +4,40 @@ windows.addEventListener('load', function(){
     form.addEventListener('submit', function(e){
         e.preventDefault();
 
-        let errores = [];
+        let errors = [];
 
         let firstName = document.getElementById('firstName')
         if(firstName.value == ''){
-            
+            errors.push('Este campo debe ser completado obligatoriamente.')
         }
 
         let lastName = document.getElementById('lastName')
         if(lastName.value == ''){
-            
+            errors.push('Este campo debe ser completado obligatoriamente.')
         }
 
         let country = document.getElementById('country')
         if(country.value == ''){//como hago aca?
-            
+            errors.push('Debes elegir tu país de residencia.')
         }
 
         let email = document.getElementById('email')
         if(email.value == ''){
-            
+            errors.push('El mail elegido debe ser válido.')
         }
 
         let password = document.getElementById('password')
-        if(password.value == ''){
-            
+        if(password.value.length < 8){
+            errors.push('La contraseña debe tener un minimo de 8 caracteres.')
+        }
+
+        if(errors.length > 0){
+            e.preventDefault();
+            let ulErrors = document.querySelector('.errors')
+            for (let i = 0; i < errors.length; i++) {
+                ulErrors.innerHTML += '<li>' + errors[i] + '</li>'
+                
+            }
         }
     })
 })
